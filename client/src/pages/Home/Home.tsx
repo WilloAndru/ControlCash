79e205ddd44e53da528bc087ddbfcf4c38ebf020
Willoandru <wilsonandrescriollo@gmail.com>
2025-08-02 15:12:37 -0500

    Estilos main v2

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,8 +2,31 @@ import { Link } from "react-router-dom"
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import './Home.css'
+import { useState } from "react";
 
 function Home() {
+
+  //Toda la logica del carrusel de imagenes
+  const [currentImgIndex, setcurrentImgIndex] = useState(0);
+
+  const images = ["/imgHome1.jpg", "/imgHome2.jpg", "/imgHome3.jpg"];
+
+  const indicators = images.map((_, index) => (
+    <div 
+      key={index}
+      className="indicator" 
+      style={{ background: currentImgIndex === index ? "white" : "transparent" }}
+    ></div>
+  ))
+  
+  const nextImage = () => {
+    setcurrentImgIndex((prev) => (prev + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setcurrentImgIndex((prev) => (prev - 1 + images.length) % images.length);
+  };
+
   return (
     <main>
 
@@ -24,8 +47,27 @@ function Home() {
 
       {/* Seccion de fotos */}
       <section className="photoSection">
-        <button><FaArrowLeft /></button>
-        <button><FaArrowRight /></button>
+
+        {/* Indicadores */}
+        <div className="indicatorContainer">
+          {indicators}
+        </div>
+
+        {/* Botones */}
+        <button className="left" onClick={prevImage}><FaArrowLeft /></button>
+        <button className="right" onClick={nextImage}><FaArrowRight /></button>
+
+        {/* Imagenes */}
+        <div className="slider">
+          <div 
+            className="slides"
+            style={{ transform: `translateX(-${currentImgIndex * 100}%)` }}
+          >
+            {images.map((src, i) => (
+              <img key={i} src={src} alt={`Slide ${i}`} />
+            ))}            
+          </div>
+        </div>
       </section>
     </main>
   )
