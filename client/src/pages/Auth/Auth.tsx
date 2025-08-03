465aef6c134ad435f5f43d4c3edcb5b01aac1d8e
Willoandru <wilsonandrescriollo@gmail.com>
2025-08-03 17:40:32 -0500

    AuthStyles

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -6,19 +6,27 @@ function Auth() {
     <main>
       {/* Formulario Auth */}
       <section className="containerAuth">
+        {/* Link de marca */}
         <Link to="/" className="brand">
           <img src="icon.png" alt="Icon" />
           <h3>ControlCash</h3>
         </Link>
-        <div className="textDiv">
+        {/* Div de bienvenida */}
+        <div>
           <h1>Welcome</h1>
           <p className="textGray">
             Sign up or log in with your Google account.
           </p>
         </div>
+        {/* Boton de google */}
+        <button>Google</button>
+        {/* Terminos */}
+        <p className="textGray terms">
+          By signing up, you agree to our Terms of Service and Privacy Policy.
+        </p>
       </section>
       {/* Imagen Derecha */}
-      <img src="" alt="Image Auth" />
+      <img className="imgRight" src="imgHome1.jpg" alt="Image Auth" />
     </main>
   );
 }
