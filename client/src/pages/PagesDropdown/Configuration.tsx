7fd18193fd7e24a8357ecdbd0c86dc99a9be0178
Willoandru <wilsonandrescriollo@gmail.com>
2025-08-10 13:06:56 -0500

    Configuratin styles

--- client/src/pages/PagesDropdown/Configuration.tsx
+++ client/src/pages/PagesDropdown/Configuration.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState } from "react";
 import { forwardRef } from "react"; //soporte de ref en componentes hijos
 import { IoClose } from "react-icons/io5";
 
@@ -8,54 +8,28 @@ interface ConfigurationProps {
 
 const Configuration = forwardRef<HTMLDivElement, ConfigurationProps>(
   ({ setIsShow }, ref) => {
-    const stored = localStorage.getItem("userData");
-    const userData = stored ? JSON.parse(stored) : {};
+    const [themeColor, setThemeColor] = useState(
+      localStorage.getItem("themeColor") || "White"
+    );
 
-    // Estado inicial de datos para comparar con los modificados y comprobar si hay cambios
-    const initialData = {
-      country: userData.country || "",
-      city: userData.city || "",
-      income: userData.income || "",
-    };
-
-    const [country, setCountry] = useState(userData.country || "");
-    const [city, setCity] = useState(userData.city || "");
-    const [income, setIncome] = useState(userData.income || "");
-
-    // Lista de inputs
+    // Lista de opciones
     const listDatas = [
       {
-        title: "Which country do you live in?",
-        value: country,
-        onChange: setCountry,
-      },
-      { title: "Which city do you live in?", value: city, onChange: setCity },
-      {
-        title: "What is your monthly income?",
-        value: income,
-        onChange: setIncome,
+        title: "Theme",
+        value: themeColor,
+        onChange: (e: React.ChangeEvent<HTMLSelectElement>) => {
+          setThemeColor(e.target.value);
+          localStorage.setItem("themeColor", e.target.value);
+          console.log(e.target.value);
+        },
       },
     ];
-
-    // Detectar cambios en los inputs
-    const hasChanges = useMemo(() => {
-      return (
-        country !== initialData.country ||
-        city !== initialData.city ||
-        income !== initialData.income
-      );
-    }, [country, city, income]);
-
-    // Envio del formulario
-    const saveDatas = (e: React.FormEvent) => {
-      e.preventDefault();
-      console.log("Guardando datos:", { country, city, income });
-    };
 
     return (
       <div className="pageDropdown">
         <section ref={ref}>
-          <form className="container" onSubmit={saveDatas}>
+          <div className="container">
+            {/* Boton de cierre */}
             <button
               onClick={() => setIsShow(false)}
               className="closeBtn"
@@ -65,42 +39,21 @@ const Configuration = forwardRef<HTMLDivElement, ConfigurationProps>(
             </button>
             {/* Header */}
             <header className="headerpageDropdown">
-              <h2>Custom your profile</h2>
-              <p>
-                Edit your information to get more accurate estimates and costs.
-              </p>
+              <h2>General configuration</h2>
             </header>
             {/* Inputs */}
             <main>
               {listDatas.map((item, index) => (
                 <div key={index}>
                   <h4>{item.title}</h4>
-                  <input
-                    type="text"
-                    value={item.value}
-                    onChange={(e) => item.onChange(e.target.value)}
-                  />
+                  <select value={item.value} onChange={item.onChange}>
+                    <option value="white">White</option>
+                    <option value="black">Black</option>
+                  </select>
                 </div>
               ))}
             </main>
-            {/* Parte de guardado del form */}
-            <footer className="footerpageDropdown">
-              <button
-                onClick={() => setIsShow(false)}
-                type="button"
-                className="styleBtnWhite"
-              >
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className="styleBtnBlack"
-                disabled={!hasChanges}
-              >
-                Save
-              </button>
-            </footer>
-          </form>
+          </div>
         </section>
       </div>
     );
