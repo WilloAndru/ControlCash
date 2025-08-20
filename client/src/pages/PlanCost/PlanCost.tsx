f4327c9cb22070d4cb5c047a9f5a117b0af35a5f
Willoandru <wilsonandrescriollo@gmail.com>
2025-08-20 18:00:08 -0500

    Styles PlanCost

--- client/src/pages/PlanCost/PlanCost.tsx
+++ client/src/pages/PlanCost/PlanCost.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import "./PlanCost.css";
 
 const URL = import.meta.env.VITE_API_URL;
 
@@ -13,7 +14,6 @@ function PlanCost() {
         const response = await axios.get(`${URL}/getPlans`);
         if (response.status === 200) {
           setDataPlans(response.data);
-          console.log(response.data);
         }
       } catch (error) {
         console.error(error);
@@ -24,8 +24,8 @@ function PlanCost() {
   }, []);
 
   return (
-    <main>
-      <h1>Find the plan that fits your needs.</h1>
+    <main className="plan-cost">
+      <h1>Find the plan that fits your needs</h1>
       {/* Seccion de planes */}
       <section className="plans-section">
         {dataPlans.map((item, index) => {
@@ -33,19 +33,19 @@ function PlanCost() {
             // Plan Individual
             <section key={index} className="plan">
               {/* Div con titulo y precio */}
-              <div className="sup-div">
-                <h5>{item.name}</h5>
+              <section className="sup-section">
+                <h2>{item.name}</h2>
                 <div className="cost-div">
-                  <h1>${item.price}</h1>
-                  <p className="textGray">{item.duration} per month</p>
+                  <h2>${item.price}</h2>
+                  <p>per {item.duration} month</p>
                 </div>
                 <button>Get Started</button>
-              </div>
+              </section>
               {/* Div de caracteristicas */}
               <section className="features-section">
                 <div className="sup-div">
-                  <h5>FEATURES</h5>
-                  <p className="textGray">Everything in our free plan plus</p>
+                  <h3>FEATURES</h3>
+                  <p>Everything in our free plan plus</p>
                 </div>
                 <ul>
                   {JSON.parse(item.features).map((f: string, i: number) => (
