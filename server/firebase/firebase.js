91f5f9eeac4db530cd448faa1dcb41be74730272
Willoandru <wilsonandrescriollo@gmail.com>
2025-10-08 16:50:40 -0500

    Correcion firebase en .env

--- server/firebase/firebase.js
+++ server/firebase/firebase.js
@@ -1,9 +1,16 @@
 import fs from "fs";
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url), "utf8")
-);
+let serviceAccount;
+
+if (process.env.FIREBASE_KEY) {
+  // En producción (Render)
+  serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+} else {
+  // En local (usa el archivo JSON)
+  const path = new URL("./serviceAccountKey.json", import.meta.url);
+  serviceAccount = JSON.parse(fs.readFileSync(path, "utf8"));
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
